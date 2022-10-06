343972f96c95e8cd2a6e3167b70eb31d5682631e
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-10-06 13:07:04 +0000

    Enfim aprendi a truncar, só que não precisa

--- l02/l02_a01.cs
+++ l02/l02_a01.cs
@@ -5,6 +5,15 @@ public class Program {
     double s = double.Parse(Console.ReadLine());
     double v = double.Parse(Console.ReadLine());
     double m = v * 15 / 100 + s;
+    m = m * 100;
+    int truncar = (int) m;
+    m = (double) truncar;
+    m = m / 100;
     Console.WriteLine($"TOTAL = R$ {m:f2}");
   }
 }
+/*
+JOAO
+500.00
+1230.30
+*/
