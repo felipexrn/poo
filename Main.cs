6072439785734c5a9247c6906532b1bfdb9d8f93
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-10-10 14:48:15 +0000

    solução 1042 sem laço

--- Main.cs
+++ Main.cs
@@ -2,7 +2,7 @@ using System;
 using System.Linq;
 public class Program {
   public static void Main() {
-    int alg = 0;
+    int alg = 1;
     if (alg ==0) {
       int[] v = Console.ReadLine().Split().Select(int.Parse).ToArray();
       int menor = v[0];
@@ -19,7 +19,40 @@ public class Program {
         Console.WriteLine($"{s[i]}");
       }
     } else if (alg ==1) {
-      
+      int[] v = Console.ReadLine().Split().Select(int.Parse).ToArray();
+      int menor = v[0];
+      if (v[1] < menor) {
+        menor = v[1];
+      }
+      if (v[2] < menor) {
+        menor = v[2]
+      }
+      int maior = v[0];
+      if (v[1] > maior) {
+        maior = v[1];
+      }
+      if (v[2] > maior) {
+        maior = v[2]
+      }
+      int medio = v[0];
+      if (medio >= menor && medio <= maior) {
+        medio = v[0];
+      }
+      medio = v[1];
+      if (medio >= menor && medio <= maior) {
+        medio = v[1];
+      }
+      medio = v[2];
+      if (medio >= menor && medio <= maior) {
+        medio = v[2];
+      }
+      Console.WriteLine($"{menor}");
+      Console.WriteLine($"{medio}");
+      Console.WriteLine($"{maior}");
+      Console.WriteLine();
+      Console.WriteLine($"{v[0]}");
+      Console.WriteLine($"{v[1]}");
+      Console.WriteLine($"{v[2]}");
     }
   }
 }
