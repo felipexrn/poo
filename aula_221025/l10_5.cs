334938202c241b8aabf9c607b847fefcf6a2e9df
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-07 22:58:50 +0000

    l10_5.cs resolvida

--- aula_221025/l10_5.cs
+++ aula_221025/l10_5.cs
@@ -1,29 +1,69 @@
 using System;
+using System.Linq;
 public class Data {
-  private int Dia = 1;
-  private int Mes = 1;
-  private int Ano = 1;
+  private int Dia = 1, Mes = 1,Ano = 1;
   public Data() {}
   public Data(int Dia, int Mes, int Ano) {
-    if (Ano > 0) this.Ano = Ano;
-    if (Mes > 0 && Mes < 13) this.Mes = Mes;
-    if (Dia > 0 && Dia < 32) this.Dia = Dia;
-    
+    if (ValidaData(Dia, Mes, Ano)) {
+      this.Ano = Ano;
+      this.Mes = Mes;
+      this.Dia = Dia;
+    }
   }
-  public class bool ValidaData() {
+  public Data(string Data) {
+    int[] D = Data.Split('/').Select(int.Parse).ToArray(); 
+    int Dia = D[0], Mes = D[1], Ano = D[2];
+    if (ValidaData(Dia, Mes, Ano)) {
+      this.Ano = Ano;
+      this.Mes = Mes;
+      this.Dia = Dia;
+    }
+  }
+  public void SetData(int Dia, int Mes, int Ano) {
+    if (ValidaData(Dia, Mes, Ano)) {
+      this.Ano = Ano;
+      this.Mes = Mes;
+      this.Dia = Dia;
+    }
+  }
+  public int GetDia() {
+    return Dia;
+  }
+  public int GetMes() {
+    return Mes;
+  }
+  public int GetAno() {
+    return Ano;
+  }
+  public override string ToString() {
+    return $"Dia = {Dia}\nMês = {Mes}\nAno = {Ano}";
+  }
+  public bool ValidaData(int Dia, int Mes, int Ano) {
+    bool Validacao = true;
     if ((Ano < 1) ||
     (Mes < 1 || Mes > 12) ||
-    (Dia < 1 || Dia > 31)) return false;
-    
-    return true;
+    (Dia < 1 || Dia > 31)) Validacao = false;
+    else if ((Mes / 8 + Mes) % 2 == 0) {
+      if (Dia > 30) Validacao = false;
+    }
+    if (Mes == 2) {
+      if (Ano % 4 != 0) Validacao = false;
+      else if (Ano % 100 == 0) {
+        if (Ano % 400 != 0) Validacao = false;  
+      }
+      if (Dia > 29) Validacao = false;
+    }
+    return Validacao;
   }
-  / 4
-  / 100
-  / 400
-  
 }
 public class Program {
   public static void Main() {
-    
+    Data D1 = new Data("31/09/2021");
+    Console.WriteLine($"D1\n{D1.ToString()}");
+    Data D2 = new Data();
+    D2.SetData(1, 12, 1995);
+    Console.WriteLine($"D2\n{D2.GetDia():00}/{D2.GetMes():00}/{D2.GetAno():0000}");
+    Data D3 = new Data(4, 4, 4);
+    Console.WriteLine($"D3\n{D3.ToString()}");
   }
 }
