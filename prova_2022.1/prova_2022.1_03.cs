6c429a310b8605efea6201cf03d722b07f1285eb
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-17 13:22:01 +0000

    prova_2022.1_02 em desenvolvimento

--- prova_2022.1/prova_2022.1_03.cs
+++ prova_2022.1/prova_2022.1_03.cs
@@ -1,14 +1,21 @@
 using System;
+
 public class CartaBaralho {
-  private string[] = naipes = {"espadas", "copas", "ouros", "paus"};
-  private int valor = Random.Next(int 1, int 13);
-  private string naipe = naipes[Random.Next(int 0, int 3)];
+  private string[] naipes = {"espadas", "copas", "ouros", "paus"};
+  private int valor = Aleatorio.valorCarta;
+  private string naipe = naipes[indiceNaipe];
   public override string ToString() {
-    
+    return $"Valor = {valor}\nNaipe = {naipe}";
   }
 }
 public class Program {
+
+  Random num = new Random();
+  public int valorCarta = num.Next(1, 14);
+  public int indiceNaipe = num.Next(0, 4);
+
   public static void Main() {
-    
+    CartaBaralho c1 = new CartaBaralho();
+    Console.WriteLine(c1.ToString());
   }
 }
