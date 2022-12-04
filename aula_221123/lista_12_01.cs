6b724a83499e9144792b62b71de9ea1c32468b81
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-04 17:23:57 +0000

    lista_12_01.cs resolvida

--- aula_221123/lista_12_01.cs
+++ aula_221123/lista_12_01.cs
@@ -1,1 +1,50 @@
 using System;
+public class Paciente {
+  private string nome = "Sem nome";
+  private string cpf = "Não declarado";
+  private string telefone = "Não declarado";
+  private DateTime nascimento;
+  public Paciente() {}
+  public Paciente(string n, string c, string t, DateTime nasc) {
+    this.SetNome(n);
+    this.SetCpf(c);
+    this.SetTelefone(t);
+    this.SetNascimento(nasc); 
+  }
+  public void SetNome(string n) {
+    if(n != "") nome = n;
+  }
+  public void SetCpf(string c) {
+    if(c != "") cpf = c;
+  }
+  public void SetTelefone(string t) {
+    if(t != "") telefone = t;
+  }
+  public void SetNascimento(DateTime nasc) {
+    nascimento = nasc;
+  }
+  public string Idade() {
+    DateTime hoje = DateTime.Now;
+    int ano = hoje.Year - nascimento.Year;
+    int mes = hoje.Month - nascimento.Month;
+    if(mes < 0) {
+      mes = 12 + mes;
+      ano--;
+    }
+    return ano.ToString() + " anos e " + 
+      mes.ToString() + " meses.";
+  }
+  public override string ToString() {
+    return $"Nome: {nome}\n" +
+      $"CPF: {cpf}\n" +
+      $"Telefone: {telefone}\n" +
+      $"Nascimento: {nascimento:dd/MM/yyyy}";
+  }
+}
+public class Program {
+  public static void Main() {
+    Paciente p = new Paciente("Felipe", "0000000000000", "99999999999", DateTime.Parse("2000-01-01"));
+    Console.WriteLine(p);
+    Console.WriteLine(p.Idade());
+  }
+}
