1b30b50bf53e8097538729cd54a27be01ddd7cb6
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-08 03:20:37 +0000

    prova_2_2022.1.cs resolvida

--- Main.cs
+++ Main.cs
@@ -6,7 +6,8 @@ public class Empresa {
   public string Nome {get; set;}
   private int qtd;
   public int Qtd {get {return qtd;}}
-  private int t, i;
+  private int t, i; // Não consegui usar i no while dentro do metodo Pesquisar
+  private double soma;
   public void Inserir(ContaPagar conta) {
     contas[this.qtd] = conta;
     qtd++; 
@@ -26,7 +27,7 @@ public class Empresa {
         c1[this.t] = conta;
         this.t++;
       }
-    }
+    }// Não consegui usar o while nesse método
     /*private ContaPagar[] c2 = new ContaPagar[this.t];
     while (this.i < this.t) {
       c2[this.i] = c1[this.i];
@@ -35,8 +36,13 @@ public class Empresa {
     return c1;
   }
   public double Total(int mes, int ano) {
-    
-    return mes + ano;
+    this.soma = 0;
+    foreach (ContaPagar conta in contas) {
+      if (conta.Vencimento.Month == mes && conta.Vencimento.Year == ano) {
+        this.soma += conta.Valor;
+      }
+    }
+    return this.soma;
   }
   public override string ToString() {
     return $"Contas: {this.contas}\n" +
@@ -53,9 +59,9 @@ public class ContaPagar : IComparable {
     return this.Vencimento.CompareTo(c2.Vencimento);
   }
   public override string ToString() {
-    return $"Fornecedor: {this.Fornecedor}\n" +
-      $"Vencimento: {this.Vencimento:dd/MM/yyyy}\n" +
-      $"Valor: R$ {this.Valor:0.00}";
+    return $"  Fornecedor: {this.Fornecedor}\n" +
+      $"  Vencimento: {this.Vencimento:dd/MM/yyyy}\n" +
+      $"  Valor: R$ {this.Valor:0.00}";
   } 
 }
 public class ComparadorFornecedor : IComparer {
@@ -69,22 +75,32 @@ public class Program {
   public static void Main() {
     ContaPagar[] contas = new ContaPagar[4];
     Empresa e1 = new Empresa {Nome = "Taberna Records", Contas = contas};
-    ContaPagar c1 = new ContaPagar {Fornecedor = "Zorzal Produções", Vencimento = DateTime.Parse("2022-12-25"), Valor = 300};
-    ContaPagar c2 = new ContaPagar {Fornecedor = "CDH Cast", Vencimento = DateTime.Parse("2022-12-11"), Valor = 600};
-    ContaPagar c3 = new ContaPagar {Fornecedor = "New Blueburry", Vencimento = DateTime.Parse("2022-11-03"), Valor = 39};
-    ContaPagar c4 = new ContaPagar {Fornecedor = "Blueburry", Vencimento = DateTime.Parse("2022-11-03"), Valor = 60};
+    ContaPagar c1 = new ContaPagar {Fornecedor = "Zorzal Produções", Vencimento = DateTime.Parse("2022-12-25"), Valor = 302.45};
+    ContaPagar c2 = new ContaPagar {Fornecedor = "CDH Cast", Vencimento = DateTime.Parse("2022-12-11"), Valor = 639.93};
+    ContaPagar c3 = new ContaPagar {Fornecedor = "New Blueburry", Vencimento = DateTime.Parse("2022-11-03"), Valor = 39.91};
+    ContaPagar c4 = new ContaPagar {Fornecedor = "Blueburry", Vencimento = DateTime.Parse("2022-11-03"), Valor = 60.57};
+    
     e1.Inserir(c1);
     e1.Inserir(c2);
     e1.Inserir(c3);
     e1.Inserir(c4);
+    
     ContaPagar[] lista1 = e1.Listar();
     ContaPagar[] lista2 = e1.Pesquisar(11, 2022);
+    double total = e1.Total(12, 2022);
+    
+    Console.WriteLine("Listar:\n");
     foreach (ContaPagar conta in lista1) {
       Console.WriteLine($"{conta}\n");
     }
+
+    Console.WriteLine("Pesquisar: \n");
     foreach (ContaPagar conta in lista2) {
-      Console.WriteLine($"{conta}\n");
+      if (conta != null)
+        Console.WriteLine($"{conta}\n");
     }
+    
+    Console.WriteLine($"Total: R$ {total:0.00}\n");
     
   }
 }
