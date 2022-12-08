4d2d66ad0cb6e2813d21be5fe27d99c5af947351
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-08 11:34:35 +0000

    etapa final

--- Main.cs
+++ Main.cs
@@ -37,6 +37,78 @@ public class Jogador : IComparable{
   }
 }
 
+// Questão 02
+
+public class Equipe {
+  private string pais;
+  private int max = 22;
+  private Jogador[] jogs;
+  private int qtd;
+  private int i;
+  public Jogador[]
+  public int Max {set{if (value > 0) max = value;}}
+  public string Pais {
+    set {if (value != "") pais = value;}
+    get {return pais;}
+  }
+  public Equipe(string n, int m) {
+    this.Pais = n;
+    this.Max = m;
+  }
+  public void Inserir(Jogador j) {
+    jogs[this.qtd] = j;
+    this.qtd++;
+  }
+  public Jogador[] Listar() {
+    Jogador[] alfab = new Jogador[this.qtd];
+    Array.Sort(jogs);
+    foreach(Jogador j in alfab)
+      j = jogs[this.i];
+    this.i = 0;
+    return jogs;
+  }
+  public Jogador[] Artilheiros() {
+    Jogador[] artilheiros = new Jogador[3];
+    CompArt comparador = new CompArt();
+    Array.Sort(jogs, comparador);
+    foreach(Jogador art in artilheiros)
+      art = jogs[this.i];
+    this.i = 0;
+    return artilheiros;
+  }
+  public Jogador[] Camisas() {
+    Jogador[] camisas = new Jogador[this.qtd];
+    CompCam comparador = new CompCam();
+    Array.Sort(jogs, comparador);
+    foreach(Jogador cam in camisas)
+      cam = jogs[this.i];
+    this.i = 0;
+    return camisas;
+  }
+  public override string ToString() {
+    return $"Pais: {pais}\n" +
+      $"Quantidade de jogadores: {qtd}";
+  }
+}
+
+// Questão 03
+
+public class CompArt : IComparer {
+  public int Compare(object obj1, object obj2) {
+    Jogador j1 = (Jogador) obj1;
+    Jogador j2 = (Jogador) obj2;
+    return j1.NumGols.CompareTo(j2.NumGols);
+  }
+}
+
+public class CompCam : IComparer {
+  public int Compare(object obj1, object obj2) {
+    Jogador j1 = (Jogador) obj1;
+    Jogador j2 = (Jogador) obj2;
+    return j1.Camisa.CompareTo(j2.Camisa);
+  }
+}
+
 // Questão 04
 
 public class Program {
@@ -48,15 +120,18 @@ public class Program {
     Jogador j4 = new Jogador("Cleuza", 2, 2);
 
     Jogador[] jogadores = new Jogador[4];
+
+    Equipe tabajara = new Equipe("Tabajara", 4);
     
-    jogadores[0] = j1;
-    jogadores[1] = j2;
-    jogadores[2] = j3;
-    jogadores[3] = j4;
+    tabajara.Inserir(j1);
+    tabajara.Inserir(j2);
+    tabajara.Inserir(j3);
+    tabajara.Inserir(j4);
     
-    Array.Sort(jogadores);
-    
+    Console.WriteLine(tabajara);
+
     foreach (Jogador jogador in jogadores)
       Console.WriteLine($"{jogador}\n");
+    
   }
 }
