992ca6f8ac2462275d229e081e92cb59a9236153
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-08 03:41:32 +0000

    prova_2_2022.1.cs resolvida e comentada

--- Main.cs
+++ Main.cs
@@ -1,10 +1,15 @@
 // Neste documento você encontrará a resolução das 3 questões da 2ª prova de 2021.1
 
-// Resolução da questão 01, que valia 35 pontos:
+// Atenção para a ordem do documento:
+// Questão 02
+// Questão 01
+// Questão 03
+
+// Resolução da questão 02, que valia 35 pontos:
 
 using System;
 using System.Collections;
-public class Empresa {
+public class Empresa { // classe que rege os objetos que representam empresas
   private ContaPagar[] contas;
   public ContaPagar[] Contas {set {contas = value;}}
   public string Nome {get; set;}
@@ -16,13 +21,13 @@ public class Empresa {
     contas[this.qtd] = conta;
     qtd++; 
   }
-  public ContaPagar[] Listar() {
+  public ContaPagar[] Listar() { // ordena lista de contas a pagar por Vencimento e Fornevedor (para desempate)
     ComparadorFornecedor objeto = new ComparadorFornecedor();
     Array.Sort(contas, objeto);
     Array.Sort(contas);
     return contas;
   }
-  public ContaPagar[] Pesquisar(int mes, int ano) {
+  public ContaPagar[] Pesquisar(int mes, int ano) { // retorna lista de contas de determinada data
     ContaPagar[] c1 = new ContaPagar[qtd];
     this.t = 0;
     foreach (ContaPagar conta in contas) {
@@ -33,7 +38,7 @@ public class Empresa {
     }
     return c1;
   }
-  public double Total(int mes, int ano) {
+  public double Total(int mes, int ano) { // retorna soma dos valores das contas de determinada data
     this.soma = 0;
     foreach (ContaPagar conta in contas) {
       if (conta.Vencimento.Month == mes && conta.Vencimento.Year == ano) {
@@ -42,15 +47,15 @@ public class Empresa {
     }
     return this.soma;
   }
-  public override string ToString() {
+  public override string ToString() { // Atributos da classe Empresa
     return $"  Nome: {this.Nome}\n" +
       $"  Quantidade contas a pagar: {this.Qtd}";
   } 
 }
 
-// Essa é a resolução da questão 02, que valia 35 pontos:
+// Essa é a resolução da questão 01, que valia 35 pontos:
 
-public class ContaPagar : IComparable {
+public class ContaPagar : IComparable { // Interface para ordenação por DateTime
   public string Fornecedor {get; set;}
   public DateTime Vencimento {get; set;}
   public double Valor {get; set;}
@@ -58,13 +63,13 @@ public class ContaPagar : IComparable {
     ContaPagar c2 = (ContaPagar) obj;
     return this.Vencimento.CompareTo(c2.Vencimento);
   }
-  public override string ToString() {
+  public override string ToString() { // Atributos da classe ContaPagar
     return $"  Fornecedor: {this.Fornecedor}\n" +
       $"  Vencimento: {this.Vencimento:dd/MM/yyyy}\n" +
       $"  Valor: R$ {this.Valor:0.00}";
   } 
 }
-public class ComparadorFornecedor : IComparer {
+public class ComparadorFornecedor : IComparer { // Interface para ordenação por string
   public int Compare(object obj1, object obj2) {
     ContaPagar c1 = (ContaPagar) obj1;
     ContaPagar c2 = (ContaPagar) obj2;
@@ -75,22 +80,35 @@ public class ComparadorFornecedor : IComparer {
 // Essa é a resolução da questão 03, que valia 30 pontos.
 
 public class Program {
-  public static void Main() {
-    ContaPagar[] contas = new ContaPagar[4];
+  public static void Main() { // classe principal para testar as classes
+    
+    ContaPagar[] contas = new ContaPagar[4]; // vetor de contas a pagar
+
+    // criação do objeto empresa
+    
     Empresa e1 = new Empresa {Nome = "Taberna Records", Contas = contas};
+
+    // criação de objetos de contas a pagar
+    
     ContaPagar c1 = new ContaPagar {Fornecedor = "Zorzal Produções", Vencimento = DateTime.Parse("2022-12-25"), Valor = 302.45};
     ContaPagar c2 = new ContaPagar {Fornecedor = "CDH Cast", Vencimento = DateTime.Parse("2022-12-11"), Valor = 639.93};
     ContaPagar c3 = new ContaPagar {Fornecedor = "New Blueburry", Vencimento = DateTime.Parse("2022-11-03"), Valor = 39.91};
     ContaPagar c4 = new ContaPagar {Fornecedor = "Blueburry", Vencimento = DateTime.Parse("2022-11-03"), Valor = 60.57};
+
+    // inserção de objetos contas a pagar no vetor de contas
     
     e1.Inserir(c1);
     e1.Inserir(c2);
     e1.Inserir(c3);
     e1.Inserir(c4);
+
+    // métodos da classe empresa
     
     ContaPagar[] lista1 = e1.Listar();
     ContaPagar[] lista2 = e1.Pesquisar(11, 2022);
     double total = e1.Total(12, 2022);
+
+    // impressão dos resultados
 
     Console.WriteLine($"Empresa:\n{e1}\n");
     
